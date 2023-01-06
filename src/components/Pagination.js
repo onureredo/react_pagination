e31ab6999a4ec21b38f47dd6927dba17f8b8a704
Onur Eredo <onureredo@yahoo.com>
2023-01-06 16:31:33 +0100

    pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,22 +1,24 @@
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
-    const pageNumbers = [];
+    
+  const pageNumbers = Array.from( Array(Math.ceil(totalPosts / postsPerPage)),(el, i) => i + 1 )
 
-    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-        pageNumbers.push(i);
-      }
+//   const pageNumbers = Array(Math.ceil(totalPosts / postsPerPage))
+//   .fill()
+//   .map((el, i) => i + 1);
 
   return (
-    <div className='pagination'>
-        <ul>
-            {pageNumbers.map(number => (
-                <li key={number}>
-                    <a onClick={() => paginate(number)} href="#">{number}</a>
-                </li>
-            ))}
-        </ul>
-      
+    <div className="pagination">
+      <ul>
+        {pageNumbers.map((number) => (
+          <li key={number}>
+            <a onClick={() => paginate(number)} href="#">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
